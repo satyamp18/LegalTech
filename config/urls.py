8bfac8dc28c222fb4018e8e14580d8a9cb1d17af
satyam Pandey <satyampandey200317@gmail.com>
2026-07-21 23:25:41 +0530

    Setup authentication module foundation and project templates

--- config/urls.py
+++ config/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path("", include("common.urls")),
+    path("accounts/", include("apps.accounts.urls")),
 ]
 
 if settings.DEBUG:
